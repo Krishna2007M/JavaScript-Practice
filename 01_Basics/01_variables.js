ce7b0d3c66da20667937e9ac08644925ba5e786c
Krishna Doot <krishnadoot9@gmail.com>
2026-02-15 07:01:07 +0000

    variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,9 @@
+const acc_Id = 144
+let acc_Email = "krishna@gmail.com"
+var acc_pass = "12345"
+acc_City = "delhi"
+
+acc_Email = "kris@gamil.com"
+acc_pass = "1123456"
+acc_City = "jaipur"
+console.table([acc_Id, acc_Email, acc_pass, acc_City])
